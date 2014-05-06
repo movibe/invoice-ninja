2ff6bdf7e957e020b81d7359b5ef52d1db3288e2
Willian Ribeiro Angelo <agfoccus@gmail.com>
2014-05-06 16:31:30 -0300

    Mysql

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'ninja_laravel',
 			'username'  => 'root',
-			'password'  => 'kemera',
+			'password'  => 'mysql',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
